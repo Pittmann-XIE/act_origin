78effc6db3c403e52e4bc64e1e691fd8786e9594
xie1 <pengtao.xie@hhi.fraunhofer.de>
2025-12-01 16:47:29 +0000

    adapt for my data

--- detr/util/misc.py
+++ detr/util/misc.py
@@ -18,10 +18,20 @@ import torch.distributed as dist
 from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
+# import torchvision
+# if version.parse(torchvision.__version__) < version.parse('0.7'):
+#     from torchvision.ops import _new_empty_tensor
+#     from torchvision.ops.misc import _output_size
+
 import torchvision
 if version.parse(torchvision.__version__) < version.parse('0.7'):
-    from torchvision.ops import _new_empty_tensor
-    from torchvision.ops.misc import _output_size
+    try:
+        from torchvision.ops import _new_empty_tensor
+        from torchvision.ops.misc import _output_size
+    except ImportError:
+        # Fallback for newer torchvision versions
+        _new_empty_tensor = None
+        _output_size = None
 
 
 class SmoothedValue(object):
@@ -446,6 +456,26 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].view(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
+
+
+# def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corners=None):
+#     # type: (Tensor, Optional[List[int]], Optional[float], str, Optional[bool]) -> Tensor
+#     """
+#     Equivalent to nn.functional.interpolate, but with support for empty batch sizes.
+#     This will eventually be supported natively by PyTorch, and this
+#     class can go away.
+#     """
+#     if version.parse(torchvision.__version__) < version.parse('0.7'):
+#         if input.numel() > 0:
+#             return torch.nn.functional.interpolate(
+#                 input, size, scale_factor, mode, align_corners
+#             )
+
+#         output_shape = _output_size(2, input, size, scale_factor)
+#         output_shape = list(input.shape[:-2]) + list(output_shape)
+#         return _new_empty_tensor(input, output_shape)
+#     else:
+#         return torchvision.ops.misc.interpolate(input, size, scale_factor, mode, align_corners)
 
 
 def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corners=None):
